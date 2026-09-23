a5743ac9e87376eeb2d04e333d9a870975ca2ada
Pavan K L <pavankl275@gmail.com>
2026-09-23 22:40:57 +0530

    updated system content

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ messages=[
         {
             "role":"system",
             "content": """
-    You are a Python technical interviewer for a fresher.
-    Ask one interview question at a time.
-    Do not give the answer immediately.
-    After the candidate answers, evaluate their answer briefly
-    and then ask the next question.
+You are a python interviewer interviewing a fresher
+rules:
+1,Ask one question at a time
+2,start with python basic questions
+3,gradually increase the difficulty level
+4,after the candidate answers,breifly evaluate the answer 
+5,Then ask the next question
+6,Do not give answer immediately
+7,Focus on python,OOP,SQL,AI/ML and software development
+
     """
         },
         {
@@ -22,6 +27,7 @@ messages=[
 print(f"Interview as been started (Type 'quit to exit)")
 question_count=0
 max_question=5
+
 while True:
     try:
         question_count+=1
@@ -34,9 +40,6 @@ while True:
         candidate_answer=input("Your Answer : ")
         if candidate_answer.strip().lower()=="quit":
             print("Ending the Interviiew GoodLuck!")
-            continue
-        elif question_count==max_question:
-            print("You answered Maximum questions")
             break
         messages.append({
             "role":"assistant",
@@ -46,6 +49,9 @@ while True:
             "role":"user",
             "content":candidate_answer
         })
+        if question_count==max_question:
+            print("\n Interview Completed")
+            break
 
     except Exception as e:
         print(f"An error occured:{e}")
